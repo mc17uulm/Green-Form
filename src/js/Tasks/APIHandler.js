27f75ba6865b80a587acf2acaa7b8e3a5453b4e9
mc17uulm <marco.combosch@uni-ulm.de>
2019-04-03 20:52:10 +0200

    changed fetch api to vanilla to work on IE, edge and safari

--- src/js/Tasks/APIHandler.js
+++ src/js/Tasks/APIHandler.js
@@ -13,29 +13,55 @@ class APIHandler
 
     static async get(res)
     {
-        const resp = await fetch(this.base + res, {
-            headers: {
-                "CsrfToken": this.get_token()
-            }
-        });
-        const json = await resp.json();
+        // const resp = await fetch(this.base + res, {
+        //     headers: {
+        //         "CsrfToken": this.get_token()
+        //     }
+        // });
+        const resp = await this.request("GET", this.base + res, {"CsrfToken": this.get_token()});
+        const json = JSON.parse(resp);
         return json;
     }
 
     static async post(res, data)
     {
-        const resp = await fetch(this.base + res, 
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "CsrfToken": this.get_token()
-                },
-                body: JSON.stringify(data)
-            }
-        );
-        const json = await resp.json();
+        // const resp = await fetch(this.base + res, 
+        //     {
+        //         method: "POST",
+        //         headers: {
+        //             "Content-Type": "application/json",
+        //             "CsrfToken": this.get_token()
+        //         },
+        //         body: JSON.stringify(data)
+        //     }
+        // );
+        const resp = await this.request("POST", this.base + res, {
+            "Content-Type": "application/json",
+            "CsrfToken": this.get_token()
+        }, JSON.stringify(data));
+        const json = JSON.parse(resp);
         return json;
+    }
+
+    static async request(type, url, headers, body = "") {
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest();
+            xhr.open(type, url);
+            Object.keys(headers).forEach(key => {
+                xhr.setRequestHeader(key, headers[key]);
+            });
+            xhr.onload = () => {
+                if(xhr.status >= 200 && xhr.status < 300) {
+                    resolve(xhr.response);
+                } else {
+                    reject({status: xhr.status, statusText: xhr.statusText});
+                }
+            };
+            xhr.onerror = () => {
+                reject({status: xhr.status, statusText: xhr.statusText});
+            };
+            xhr.send(body);
+        });
     }
 
 }
